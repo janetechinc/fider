849b70018c51e8f2552124a3f2c52ab7a20bf21a
goenning <me@goenning.net>
2021-04-18 10:54:55 +0100

    refactor

--- public/components/Header.tsx
+++ public/components/Header.tsx
@@ -19,24 +19,26 @@ export const Header = () => {
       <DevBanner />
       <div id="c-header">
         <SignInModal isOpen={isSignInModalOpen} onClose={hideModal} />
-        <HStack className="c-menu shadow p-2">
-          <HStack justify="between" className="container">
-            <a href="/" className="flex flex-x flex--spacing-2 h-8">
-              <TenantLogo size={100} />
-              <h1 className="text-title">{fider.session.tenant.name}</h1>
-            </a>
-            {fider.session.isAuthenticated && (
-              <HStack spacing={2}>
-                <NotificationIndicator />
-                <UserMenu />
-              </HStack>
-            )}
-            {!fider.session.isAuthenticated && (
-              <a href="#" className="uppercase text-caption" onClick={showModal}>
-                Sign in
+        <HStack className="c-menu shadow p-2 w-full">
+          <div className="container">
+            <HStack justify="between">
+              <a href="/" className="flex flex-x flex--spacing-2 h-8">
+                <TenantLogo size={100} />
+                <h1 className="text-title">{fider.session.tenant.name}</h1>
               </a>
-            )}
-          </HStack>
+              {fider.session.isAuthenticated && (
+                <HStack spacing={2}>
+                  <NotificationIndicator />
+                  <UserMenu />
+                </HStack>
+              )}
+              {!fider.session.isAuthenticated && (
+                <a href="#" className="uppercase text-caption" onClick={showModal}>
+                  Sign in
+                </a>
+              )}
+            </HStack>
+          </div>
         </HStack>
       </div>
     </>
